04f46f66db53a9b8428cb2f932db27b483b20fe6
jicf <jicf@zylliondata.com>
2019-06-27 13:26:28 +0800

    second commit

---
+++ pages/dataset-list/_industry/_category.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>2</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$nuxt.$route);
+  }
+};
+</script>
+
+// {
+//   fullPath: "/dataset-list/f/f",
+//   hash: "",
+//   matched: [{}],
+//   meta: {},
+//   name: "dataset-list-industry-category",
+//   path: "/dataset-list/f/f",
+//   query: {}
+// }
